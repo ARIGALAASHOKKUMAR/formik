a065bf17140b743f525b241c8529c67281326357
ARIGALA ASHOK KUMAR <arigelaashok123@gmail.com>
2024-02-23 12:34:58 +0530

    first-commit

---
+++ first/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [name, setName] = useState("ashok");
+  const [count,setcount]=useState(1)
+
+  const isEmpty =name.length === 0;
+  console.log(name);
+  const func = () => {
+    const inputs = [];
+    for (let i = 0; i < count; i++) {
+      inputs.push(<div><input key={i} type='text' value={isEmpty ? undefined : name}/></div>);
+    }
+    return <div>{inputs}</div>;
+  };
+  return (
+    <div>
+      {func()}
+      <button onClick={()=>setcount(count+1)}>+</button>
+    </div>
+  );
+};
+export default App;
